c933bcecbbc2498a5dee58ba81aa4f3e5771516e
lijinwei <lijinwei_123@126.com>
2018-03-06 13:57:00 +0800

    设计模式14 模板方法模式

--- src/c/k/dp/strategy/Client.java
+++ src/c/k/dp/strategy/Client.java
@@ -5,6 +5,15 @@ import c.k.dp.strategy.impl.Multiply;
 import c.k.dp.strategy.impl.Plus;
 import c.k.dp.strategy.interf.ICalculator;
 
+/**
+ * 策略模式定义了一系列算法，并将每个算法封装起来，
+ * 使他们可以相互替换，且算法的变化不会影响到使用算法的客户。
+ *
+ * 需要设计一个接口，为一系列实现类提供统一的方法，多个实现类实现该接口
+ *
+ * 策略模式的决定权在用户，系统本身提供不同算法的实现，新增或者删除算法，
+ * 对各种算法做封装。因此，策略模式多用在算法决策系统中，外部用户只需要决定用哪个算法即可。
+ */
 public class Client {
     public int plus(String exp){
         ICalculator cal = new Plus();
