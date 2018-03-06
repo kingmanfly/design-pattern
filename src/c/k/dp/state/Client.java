de2ecb172cabef5542bbfbc80e1c5b3f2468dec9
lijinwei <lijinwei_123@126.com>
2018-03-06 16:53:14 +0800

    设计模式20 状态模式

---
+++ src/c/k/dp/state/Client.java
@@ -0,0 +1,16 @@
+package c.k.dp.state;
+
+public class Client {
+    public void use(){
+        State state = new State();
+        Context context = new Context(state);
+
+        //设置第一种状态
+        state.setValue("state1");
+        context.method();
+
+        //设置第二种状态
+        state.setValue("state2");
+        context.method();
+    }
+}
---
+++ test/c/k/dp/state/ClientTest.java
@@ -0,0 +1,14 @@
+package c.k.dp.state;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClientTest {
+
+    @Test
+    public void use() {
+        Client client = new Client();
+        client.use();
+    }
+}
